2bd94c313243b75cf7625dd985d8b433594170e8
Carlos Filoteo <c.filoteo12@gmail.com>
2020-03-18 00:04:36 -0600

    Update App to render simple demo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <FocusVisible elem="main">
+    <p tabindex="0">Focusable paragraph</p>
+    <input type="text" />
+    <button type="button">A button</button>
+  </FocusVisible>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import FocusVisible from "./VisibleFocus";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    FocusVisible
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
