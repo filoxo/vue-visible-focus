4d25c1fad625bfdfcee9ee03146f2eeb691de376
Carlos Filoteo <c.filoteo12@gmail.com>
2020-03-25 00:43:59 -0600

    Copy over HTML from react-visible-focus

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
-  <FocusVisible elem="main">
-    <p tabindex="0">Focusable paragraph</p>
-    <input type="text" />
-    <button type="button">A button</button>
-  </FocusVisible>
+  <main>
+    <FocusVisible>
+      <h1>vue-visible-focus</h1>
+      <h2 tabindex="0">focusable subheader (click on me)</h2>
+      <input type="text" placeholder="An input" />
+      <button type="button">A button</button>
+    </FocusVisible>
+    <p>
+      This component attempts to emulate the
+      <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/:focus-visible"
+        >:focus-visible pseudoselector</a
+      >
+      behavior using similar heuristics but does not follow the spec completely.
+    </p>
+    <p>Focus styles are still displayed when focus is given to an input.</p>
+  </main>
 </template>
 
 <script>
