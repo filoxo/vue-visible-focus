1350850bb959b89ac0d92d36d039cb66f8ccfb4c
Carlos Filoteo <c.filoteo12@gmail.com>
2020-04-04 23:26:38 -0600

    Add test for switching from mouse to keyboard modality

--- tests/e2e/specs/visible-focus.spec.js
+++ tests/e2e/specs/visible-focus.spec.js
@@ -1,57 +1,96 @@
-describe('VisibleFocus', () => {
+describe("VisibleFocus", () => {
   const none = /none/i
   const notNone = /^((?!none).)*$/i
 
   beforeEach(() => {
-    cy.visit("/");
-  })
-  
-  describe('hides focus ring when clicking', () => {
-    
-    it('on a focusable element (button)', () => {
-      cy.findByText('A button').should('exist').click()
-      cy.focused().should('have.css', 'outline').and('match', none)
-    })
-    
-    it('on a div with tabindex', () => {
-      cy.findByText(/focusable subheader/i).should('exist').click()
-      cy.focused().should('have.css', 'outline').and('match', none)
-    })
+    cy.visit("/")
   })
 
-  describe('shows focus ring when', () => {
-
-    it('clicking on an input element', () => {
-      cy.findByPlaceholderText('An input').should('exist').click()
-      cy.focused().should('have.css', 'outline').and('match', notNone)
-    })
-    
-    it('pressing Tab', () => {
-      cy.get("body").tab()
-      cy.focused().should('have.css', 'outline').and('match', notNone)
-      cy.focused().tab()
-      cy.focused().should('have.css', 'outline').and('match', notNone)
-      cy.focused().tab()
-      cy.focused().should('have.css', 'outline').and('match', notNone)
-    })
-    
-    it('pressing arrow keys', () => {
-      cy.findByText('A button').focus()
-      cy.focused().type('{uparrow}')
-      cy.focused().should('have.css', 'outline').and('match', notNone)
-      cy.focused().type('{downarrow}')
-      cy.focused().should('have.css', 'outline').and('match', notNone)
-      cy.focused().type('{rightarrow}')
-      cy.focused().should('have.css', 'outline').and('match', notNone)
-      cy.focused().type('{leftarrow}')
-      cy.focused().should('have.css', 'outline').and('match', notNone)
+  describe("hides focus ring when clicking", () => {
+    it("on a focusable element (button)", () => {
+      cy.findByText("A button")
+        .should("exist")
+        .click()
+      cy.focused()
+        .should("have.css", "outline")
+        .and("match", none)
     })
 
-    it('switching from mouse to keyboard modality', () => {
-      cy.findByText(/focusable subheader/i).focus ()
-      cy.focused().should('have.css', 'outline').and('match', notNone)
+    it("on a div with tabindex", () => {
+      cy.findByText(/focusable subheader/i)
+        .should("exist")
+        .click()
+      cy.focused()
+        .should("have.css", "outline")
+        .and("match", none)
+    })
+
+    it("switching from keyboard to mouse modality", () => {
+      cy.findByText(/focusable subheader/i).focus()
+      cy.focused()
+        .should("have.css", "outline")
+        .and("match", notNone)
       cy.focused().click()
-      cy.focused().should('have.css', 'outline').and('match', none)
+      cy.focused()
+        .should("have.css", "outline")
+        .and("match", none)
     })
   })
-} )
+
+  describe("shows focus ring when", () => {
+    it("clicking on an input element", () => {
+      cy.findByPlaceholderText("An input")
+        .should("exist")
+        .click()
+      cy.focused()
+        .should("have.css", "outline")
+        .and("match", notNone)
+    })
+
+    it("pressing Tab", () => {
+      cy.get("body").tab()
+      cy.focused()
+        .should("have.css", "outline")
+        .and("match", notNone)
+      cy.focused().tab()
+      cy.focused()
+        .should("have.css", "outline")
+        .and("match", notNone)
+      cy.focused().tab()
+      cy.focused()
+        .should("have.css", "outline")
+        .and("match", notNone)
+    })
+
+    it("pressing arrow keys", () => {
+      cy.findByText("A button").focus()
+      cy.focused().type("{uparrow}")
+      cy.focused()
+        .should("have.css", "outline")
+        .and("match", notNone)
+      cy.focused().type("{downarrow}")
+      cy.focused()
+        .should("have.css", "outline")
+        .and("match", notNone)
+      cy.focused().type("{rightarrow}")
+      cy.focused()
+        .should("have.css", "outline")
+        .and("match", notNone)
+      cy.focused().type("{leftarrow}")
+      cy.focused()
+        .should("have.css", "outline")
+        .and("match", notNone)
+    })
+
+    it("switching from mouse to keyboard modality", () => {
+      cy.findByText(/focusable subheader/i).click()
+      cy.get("body")
+        .click(10, 10)
+        .tab()
+      // cy.findByText(/focusable subheader/i).focus()
+      cy.focused()
+        .should("have.css", "outline")
+        .and("match", notNone)
+    })
+  })
+})
